c50ea751b8d55a8ba66a8fccdfc459aa073991c3
Ramtin Soltani <ramtinsoltani93@gmail.com>
2023-02-26 13:33:09 -0800

    fixed dynamic port issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { ExpressPeerServer } = require('peer');
 const app = express();
 const FE_ROOT_DIR = path.resolve(__dirname, 'dist', 'connect-the-dots-game');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
 
